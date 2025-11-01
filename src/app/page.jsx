325be593cf3184a4f02b988b756fcd535ec7deb4
oldiney <oldineygalvao@gmail.com>
2025-11-01 10:33:01 -0300

    feat: Estruturação do projeto

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import styles from './page.module.css';
+
+export default function Home(){
+  return(
+    <div className="container">
+      <h1 className={styles.tituloHome}>Título 1</h1>
+      <h2>Título 2</h2>
+      <h3>Título 3</h3>
+      <p className="txtDestaque">Primeiro exemplo no Next</p>
+      <p>Primeiro exemplo no Next</p>
+      <small>Texto menor</small>
+      <br />
+      <div className="quadrado"></div>
+    </div>
+  );
+}
